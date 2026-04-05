4b66c422d49e688ff18ca5e3fd8d7307bccc071e
Seowoo Kang <moonsunmoor@gmail.com>
2026-04-05 12:36:34 +0900

    flask 외부 접속 허용, 디버깅 모드 설정, 포트 명시화

--- route-engine/hello.py
+++ route-engine/hello.py
@@ -7,4 +7,6 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000, debug=True)
+else:
+    print("Failed to run Flask app. It is not '__main__'.")
